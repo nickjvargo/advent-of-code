3eb4cf518383aef93f9a53ba28875f6627f72471
Nick Vargo <nickjvargo@gmail.com>
2025-12-06 07:26:17 -0500

    going good

--- 2025/day-one/solution.js
+++ 2025/day-one/solution.js
@@ -1,5 +1,18 @@
+const fs = require('node:fs');
 
+fs.readFile('2025/day-one/sample.txt', 'utf8', (err, data) => {
+  if (err) {
+    console.error('How could this even err??', err);
+    return;
+  }
+  safeCracker(data);
+});
 
-function() {
-
+function safeCracker(input) {
+    const dialStartingPosition = 50;
+    const dial = new LinkedList();
+    // L = go left, subtract
+    // R = go right, add
 }
+
+LinkedList
